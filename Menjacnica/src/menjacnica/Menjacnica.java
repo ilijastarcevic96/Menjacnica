5195a9527336f3a3f68da74da60bae8ed769e92d
ilijastarcevic96 <starcevicfon@gmail.com>
2017-03-25 22:49:31 +0100

    U grani master refaktorisana metoda dodajKurs

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,15 +1,29 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta> kursevi = new LinkedList<Valuta>();
+	
 	@Override
 	public void dodajKurs(double prodajniKurs, double kupovniKurs, double srednjiKurs, GregorianCalendar datum,
 			String naziv, String skraceniNaziv) {
 
+		if(prodajniKurs == 0 || kupovniKurs == 0 || srednjiKurs == 0 || datum == null || naziv == null || skraceniNaziv == null){
+			System.out.println("Uneli ste neki parametar pogresno");
+			return;
+		}
+		
+		Valuta valuta = new Valuta(naziv, skraceniNaziv, datum, prodajniKurs, kupovniKurs, srednjiKurs);
+		
+		if(!kursevi.contains(valuta)){
+			kursevi.add(valuta);
+			return;
+		}
 	}
 
 	@Override
